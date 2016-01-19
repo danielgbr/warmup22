7d5227cf6907878181c0682f537d5b4bae97c3a6
Daniel Romagnoli <daniel_goncalves@me.com>
2016-01-19 21:36:12 -0200

    Initial Commit

---
+++ Warmup22/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warmup22
+//
+//  Created by Daniel Gonçalves on 1/19/16.
+//  Copyright © 2016 Daniel Gonçalves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
